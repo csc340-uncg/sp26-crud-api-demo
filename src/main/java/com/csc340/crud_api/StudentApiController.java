2fcac286d44e9ff5ead6aa8d0cc7ba478f9f047a
csc340-uncg <uncg.csc340@gmail.com>
2026-02-26 12:01:19 -0500

    Change Controller annotation to RestController

--- src/main/java/com/csc340/crud_api/StudentApiController.java
+++ src/main/java/com/csc340/crud_api/StudentApiController.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
 @RequestMapping("/api/students")
 public class StudentApiController {
 
