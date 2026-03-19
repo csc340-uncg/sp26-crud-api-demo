64775f299ccc4c5dbfca678fb2e67f31ce6cd4d5
csc340-uncg <uncg.csc340@gmail.com>
2026-03-18 22:45:13 -0400

    Add integration tests for Student API endpoints

---
+++ src/test/java/com/csc340/crud_api/StudentApiControllerIntegrationTests.java
@@ -0,0 +1,36 @@
+package com.csc340.crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentApiControllerIntegrationTests {
+
+  @LocalServerPort
+  private int port;
+
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  @Test
+  void testCreateStudent() {
+    Student student = new Student("Test Student", "test@school.edu", "CSC", 3.0);
+
+    String url = "http://localhost:" + port + "/api/students/";
+    var response = restTemplate.postForEntity(url, student, Student.class);
+    assert (response.getStatusCode().is2xxSuccessful());
+    assert (response.getBody() != null);
+    assert (response.getBody().getName().equals("Test Student"));
+  }
+
+  @Test
+  void testGetAllStudents() {
+    var response = restTemplate.getForEntity("http://localhost:" + port + "/api/students/", Student[].class);
+    assert (response.getStatusCode().is2xxSuccessful());
+
+  }
+}
